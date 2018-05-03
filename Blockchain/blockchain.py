9d95a04cab7797741c4c72d64523377fa9e5ef36
jonngyu <johnt.nguyen88@gmail.com>
2018-05-03 17:58:00 -0400

    Adding a check for the blockchain to see if there's a starting transaction or not for it to safely append.

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -4,16 +4,21 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current block. """
+    # Creating a check to see if there's a starting transaction
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value as well as the last block value to the blockchain 
 
     Arguments:
         :transaction_amount: The amount should be added.
         :last_transaction: The last block amount (default [1]).   
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -35,10 +40,6 @@ def print_blockchain_elements():
         print(block)
 
 
-# Get the first transaction and add the value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
@@ -47,15 +48,14 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'q':
         break
     else:
         print('Input was invalid, please pick a value from the list!')
-    print ('Choice registered!')
-
+    print('Choice registered!')
 
 
 print('Done!')
