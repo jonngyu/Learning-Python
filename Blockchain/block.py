b8e30b309a646cbb2c0c9b70a80bb1c51c9f80dc
jonngyu <johnt.nguyen88@gmail.com>
2018-05-16 02:43:44 -0400

    Creating a repr that's inheritable by other classes

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -1,7 +1,10 @@
 from time import time
 
-class Block:
-    def __init__(self, index, previous_hash, transactions, proof, time= time()):
+from printable import Printable
+
+
+class Block(Printable):
+    def __init__(self, index, previous_hash, transactions, proof, time=time()):
         self.index = index
         self.previous_hash = previous_hash
         self.transactions = transactions
